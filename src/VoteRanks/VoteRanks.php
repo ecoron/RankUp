27b1b2ca848664b73821c68d439185c4a9ef75c5
Ronald Schmidt <ronald.schmidt@zu-web.de>
2015-10-10 23:49:22 +0200

    start custom VoteRewards

--- src/VoteRanks/VoteRanks.php
+++ src/VoteRanks/VoteRanks.php
@@ -6,8 +6,12 @@ use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+use VoteRanks\VoteReward\VoteReward;
+use VoteRanks\VoteReward\QueryTask;
 
 class VoteRanks extends PluginBase{
+
+    var $purePerms;
 
     public function onEnable(){
     }
@@ -15,9 +19,28 @@ class VoteRanks extends PluginBase{
     public function onDisable(){
     }
 
-    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-
+    public function onCommand(CommandSender $p,Command $cmd,$label,array $args) {
+        if(!($p instanceof Player)) {
+            $p->sendMessage("Command must be used in-game.");
+            return true;
+        }
+        if($p->hasPermission("voteranks") || $p->hasPermission("voteranks.vote")) {
+            $query = new QueryTask("http://minecraftpocket-servers.com/api/?object=votes&element=claim&key=" . $this->key . "&username=" . $p->getName(),$p->getName(),true);
+            $this->getServer()->getScheduler()->scheduleAsyncTask($query);
+        } else {
+            $p->sendMessage("You do not have permission to vote.");
+        }
         return true;
+    }
+
+    private function initPPerms() {
+        if(($plugin = $this->getServer()->getPluginManager()->getPlugin("PurePerms")) instanceof Plugin){
+            $this->purePerms = $plugin;
+            $this->getLogger()->info("Successfully loaded with PurePerms");
+        }else{
+            $this->getLogger()->alert("Dependency PurePerms not found");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+        }
     }
 
 }
--- src/VoteRanks/VoteReward/VoteReward.php
+++ src/VoteRanks/VoteReward/VoteReward.php
@@ -7,9 +7,11 @@ use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 use pocketmine\item\Item;
-class Main extends PluginBase {
-  private $items, $commands, $key, $url;
-  public function onEnable() {
+class VoteReward extends PluginBase {
+  private $commands, $key, $url;
+
+  public function __construct(){
+
     if(!file_exists($this->getDataFolder() . "config.yml")) {
       @mkdir($this->getDataFolder());
       file_put_contents($this->getDataFolder() . "config.yml",$this->getResource("config.yml"));
@@ -18,43 +20,15 @@ class Main extends PluginBase {
     $num = 0;
     $this->key = $c["API-Key"];
     $this->url = $c["Vote-URL"];
-    $rewards = $c["Rewards"];
-    foreach($rewards["Items"] as $i) {
-      $r = explode(":",$i);
-      $this->items[$num] = new Item($r[0],$r[1],$r[2]);
-      $num++;
-    }
-    $num = 0;
-    foreach($rewards["Commands"] as $i) {
-      $this->commands[$num] = $i;
-      $num++;
-    }
+
   }
-  public function onCommand(CommandSender $p,Command $cmd,$label,array $args) {
-    if(!($p instanceof Player)) {
-      $p->sendMessage("Command must be used in-game.");
-      return true;
-    }
-    if($p->hasPermission("votereward") || $p->hasPermission("votereward.vote")) {
-      $query = new QueryTask("http://minecraftpocket-servers.com/api/?object=votes&element=claim&key=" . $this->key . "&username=" . $p->getName(),$p->getName(),true);
-      $this->getServer()->getScheduler()->scheduleAsyncTask($query);
-    } else {
-      $p->sendMessage("You do not have permission to vote.");
-    }
-    return true;
-  }
+
   public function give(Player $p,$s) {
     if($s == "0") {
       $p->sendMessage("You haven't voted yet!\n" . $this->url . "\nVote now for cool rewards!");
     } else if($s == "1") {
       $query = new QueryTask("http://minecraftpocket-servers.com/api/?action=post&object=votes&element=claim&key=" . $this->key . "&username=" . $p->getName(),$p->getName(),false);
       $this->getServer()->getScheduler()->scheduleAsyncTask($query);
-      foreach($this->items as $i) {
-        $p->getInventory()->addItem($i);
-      }
-      foreach($this->commands as $i) {
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(),str_replace("{PLAYER}",$p->getName(),$i));
-      }
     } else if($s == "2") {
       $p->sendMessage("You've already voted today! Come back tomorrow to vote again.");
     } else {
